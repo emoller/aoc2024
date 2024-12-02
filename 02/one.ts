8f4de83c55ba4fea87babbdbe8776bc315d83197
Erik Möller <emoller@opera.com>
2024-12-02 08:29:09 +0100

    Day two

---
+++ 02/one.ts
@@ -0,0 +1,16 @@
+const text: String = Deno.readTextFileSync("input.txt");
+
+const res = text.split("\n").map((r) => r.split(" ").map(Number));
+let diff = res.map((r) =>
+  r.map((n, i) => (i == 0 ? 0 : n - r[i - 1])).slice(1)
+);
+
+diff.forEach((a, i) => {
+  if (a[0] < 0) {
+    diff[i] = a.map((e) => e * -1);
+  }
+});
+
+const ok = diff.filter((n) => n.every((e) => 0 < e && e < 4)).length;
+
+console.log(ok);
---
+++ 02/two.ts
@@ -0,0 +1,32 @@
+const text: String = Deno.readTextFileSync("input.txt");
+
+const res = text.split("\n").map((r) => r.split(" ").map(Number));
+
+const check = (r) => {
+  let diff = r.map((n, i) => (i == 0 ? 0 : n - r[i - 1])).slice(1);
+  if (diff[0] < 0) {
+    diff = diff.map((e) => e * -1);
+  }
+  return diff.every((e) => 0 < e && e < 4);
+};
+
+let ok = 0;
+
+for (let j = 0; j < res.length; j++) {
+  let r = res[j];
+
+  if (check(r)) {
+    ok++;
+    continue;
+  }
+  let done = false;
+  for (let i = 0; i < r.length && !done; i++) {
+    const n = [...r.slice(0, i), ...r.slice(i + 1)];
+    if (check(n)) {
+      ok++;
+      done = true;
+    }
+  }
+}
+
+console.log(ok);
