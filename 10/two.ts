f6acffda8a2f138d143833e0434faf8d24f6bdce
Erik Möller <emoller@opera.com>
2024-12-10 07:23:16 +0100

    Day 10

---
+++ 10/two.ts
@@ -0,0 +1,46 @@
+const text: string = Deno.readTextFileSync("input.txt");
+
+const grid = text.split("\n").map((r) => r.split(""));
+const width = grid[0].length;
+const height = grid.length;
+
+const inside = (x: number, y: number) =>
+  x >= 0 && x < width && y >= 0 && y < height;
+
+const getTrailCount = (x: number, y: number): number => {
+  const dirs = [
+    [1, 0],
+    [-1, 0],
+    [0, 1],
+    [0, -1],
+  ];
+  type Node = { x: number; y: number; v: number };
+  const queue: Node[] = [{ x, y, v: 0 }];
+  let nines = 0;
+  while (queue.length > 0) {
+    const { x, y, v } = queue.shift()!;
+    if (v === 9) {
+      nines++;
+      continue;
+    }
+    dirs.forEach(([dx, dy]) => {
+      const nx = x + dx;
+      const ny = y + dy;
+      if (inside(nx, ny) && grid[ny][nx] === (v + 1).toString()) {
+        queue.push({ x: nx, y: ny, v: v + 1 });
+      }
+    });
+  }
+  return nines;
+};
+
+let sum = 0;
+for (let y = 0; y < height; y++) {
+  for (let x = 0; x < width; x++) {
+    if (grid[y][x] === "0") {
+      sum += getTrailCount(x, y);
+    }
+  }
+}
+
+console.log(sum);
